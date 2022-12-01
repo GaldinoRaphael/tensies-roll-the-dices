14aae75d3d5ed6565649d884bb65ede4f3455cf0
raphaelgadino <raphael.galdino.s@hotmail.com>
2022-11-30 23:45:28 -0300

    feat: end-game and install confetti

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Dice from './Dice'
 import { nanoid } from 'nanoid';
-
+import Confetti from 'react-confetti';
 
 
 function App() {
+  const [tenzies, setTenzies] = useState(false)
   const [dices, setDices] = useState(() => allNewDices());
+  
+  useEffect(()=>{
+    const everyDiceIsHeld = dices.every(dice => dice.isHeld);
+    const valuesOfArray = dices.map(dice => dice.value);
+    const nonRepeatedValues = new Set(valuesOfArray);
+    const allTheDicesHasSameValue = nonRepeatedValues.size === 1 ;
+
+    setTenzies(everyDiceIsHeld && allTheDicesHasSameValue);
+  }, [dices])
+
+  useEffect(() => {
+    tenzies && console.log("you won")
+  }, [tenzies])
 
   function allNewDices(){
     const dices = [];
@@ -19,27 +33,27 @@ function App() {
   }
 
   function rollDices(){
-    setDices(() => allNewDices())
+    setDices((oldDices) => oldDices.map(dice => dice.isHeld ? dice : {...dice, value: Math.ceil(Math.random() * 6)}))
   }
 
   function heldDice(event, id){
     event.stopPropagation();
-    console.log(dices)
     setDices(oldDices => oldDices.map(dice => dice.id === id ? {...dice, isHeld: true} : dice));
   }
-  
+
   const diceElements =   dices.map(dice => <Dice number={dice.value} isHeld={dice.isHeld} key={dice.id} 
                                     id={dice.id} hold={heldDice} />)
 
   return (
     <div className="app">
+      {tenzies && <Confetti />}
       <main>
         <h1 className='title'>Tenzies</h1>
         <h2 className='subtitle'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</h2>
         <div className='dices'>
         {diceElements}
         </div>
-        <button className='button' onClick={rollDices}>ROLL</button>
+        <button className='button' onClick={rollDices}>{tenzies ? "New Game" : "ROLL"}</button>
       </main>
     </div>
   );
