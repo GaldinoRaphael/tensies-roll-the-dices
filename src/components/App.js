aa13cee3cda7ec73fb4eae7617ac7798c9b6c55d
raphaelgadino <raphael.galdino.s@hotmail.com>
2022-11-30 22:13:57 -0300

    feat: roll dices

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,37 @@
+import { useState } from 'react';
 import './App.css';
 import Dice from './Dice';
 
+
+
 function App() {
+  function allNewDices(){
+    const numbers = [];
+    for(let i = 0; i < 10;i++){
+      numbers.push(Math.ceil(Math.random() * 6))
+    }
+  
+    return numbers;
+  }
+
+  function rollDices(){
+    setDices(() => allNewDices())
+  }
+
+  const [dices, setDices] = useState(() => allNewDices());
+  
+  
+ const diceElements =   dices.map(number => <Dice number={number} />)
+
   return (
     <div className="app">
       <main>
         <h1 className='title'>Tenzies</h1>
         <h2 className='subtitle'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</h2>
-        <Dice number="1" />
-        <button className='button'>ROLL</button>
+        <div className='dices'>
+        {diceElements}
+        </div>
+        <button className='button' onClick={rollDices}>ROLL</button>
       </main>
     </div>
   );
